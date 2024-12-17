27b0723de9c8d391477d9ea8b358508d742001b2
leGit-y <99312658+leGit-y@users.noreply.github.com>
2024-12-17 14:17:45 +0900

    [Silver IV] Title: 스택, Time: 280 ms, Memory: 20176 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -10,45 +12,86 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
         String cmd;
-        Stack<Integer> stack = new Stack<>();
+//        Stack<Integer> stack = new Stack<>();
+        List<Integer> stack = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             cmd = st.nextToken();
-            switch (cmd){
-                case "push":
-                    int X = Integer.parseInt(st.nextToken());
-                    stack.push(X);
-                    break;
-
-                case "pop":
-                    if(stack.isEmpty())
-                        System.out.println("-1");
-                    else
-                        System.out.println(stack.pop());
-                    break;
-
-                case "size":
-                    System.out.println(stack.size());
-                    break;
-
-                case "empty":
-                    if(stack.isEmpty())
-                        System.out.println("1");
-                    else
-                        System.out.println("0");
-                    break;
-
-                case "top":
-                    if(stack.isEmpty())
-                        System.out.println("-1");
-                    else
-                        System.out.println(stack.peek());
-                    break;
-
-            }
+//            solution_stack(st, cmd, stack);
+            solution_arraylist(st, cmd, stack);
 
         }
 
+    }
+
+    private static void solution_arraylist(StringTokenizer st, String cmd, List<Integer> stack) {
+        switch (cmd){
+            case "push":
+                int X = Integer.parseInt(st.nextToken());
+                stack.add(X);
+                break;
+
+            case "pop":
+                if(stack.isEmpty())
+                    System.out.println("-1");
+                else
+                    System.out.println(stack.remove(stack.size()-1));
+                break;
+
+            case "size":
+                System.out.println(stack.size());
+                break;
+
+            case "empty":
+                if(stack.isEmpty())
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+                break;
+
+            case "top":
+                if(stack.isEmpty())
+                    System.out.println("-1");
+                else
+                    System.out.println(stack.get(stack.size()-1));
+                break;
+
+        }
+    }
+
+    private static void solution_stack(StringTokenizer st, String cmd, Stack<Integer> stack) {
+        switch (cmd){
+            case "push":
+                int X = Integer.parseInt(st.nextToken());
+                stack.push(X);
+                break;
+
+            case "pop":
+                if(stack.isEmpty())
+                    System.out.println("-1");
+                else
+                    System.out.println(stack.pop());
+                break;
+
+            case "size":
+                System.out.println(stack.size());
+                break;
+
+            case "empty":
+                if(stack.isEmpty())
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+                break;
+
+            case "top":
+                if(stack.isEmpty())
+                    System.out.println("-1");
+                else
+                    System.out.println(stack.peek());
+                break;
+
+        }
     }
 
 
