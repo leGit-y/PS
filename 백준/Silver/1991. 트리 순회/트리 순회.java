180ca724f7283746e4946d208aa64fbc93af1c66
leGit-y <99312658+leGit-y@users.noreply.github.com>
2024-12-23 23:28:35 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,57 +1,68 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+// 이진트리: 자식이 최대 2개인 트리
 public class Main {
-    static int[][] tree;
+    static char[][] binaryTree;
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        tree = new int[N][2];
+        N = Integer.parseInt(st.nextToken());
+        binaryTree = new char[N+1][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int n = st.nextToken().charAt(0) - 'A';
-            String l = st.nextToken();
-            String r = st.nextToken();
+            int n = st.nextToken().charAt(0) - 'A' + 1;
+            char left = st.nextToken().charAt(0);
+            char right = st.nextToken().charAt(0);
 
-            tree[n][0] = l.equals(".") ? -1 : l.charAt(0) - 'A';
-            tree[n][1] = r.equals(".") ? -1 : r.charAt(0) - 'A';
+            binaryTree[n][0] = left;
+            binaryTree[n][1] = right;
 
         }
 
-        preOrderTraversal(0);
+
+        preorder('A');
         System.out.println();
-        inOrderTraversal(0);
+        inorder('A');
         System.out.println();
-        postOrderTraversal(0);
-        System.out.println();
+        postorder('A');
 
     }
 
-    static void preOrderTraversal(int cur){
-        if (cur == -1) return;
-        System.out.print((char)(cur+'A'));
-        preOrderTraversal(tree[cur][0]);
-        preOrderTraversal(tree[cur][1]);
-    }
-
-    static void inOrderTraversal(int cur){
-        if (cur == -1) return;
-
-        inOrderTraversal(tree[cur][0]);
-        System.out.print((char)(cur+'A'));
-        inOrderTraversal(tree[cur][1]);
+    static private void preorder(char cur) {
+        if(cur == '.'){
+            return;
+        }
+        int curIdx = cur - 'A' + 1;
+        System.out.print(cur);
+        preorder(binaryTree[curIdx][0]);
+        preorder(binaryTree[curIdx][1]);
 
     }
 
-    static void postOrderTraversal(int cur){
-        if (cur == -1) return;
+    static private void inorder(char cur){
+        if(cur == '.'){
+            return;
+        }
+        int curIdx = cur - 'A' + 1;
 
-        postOrderTraversal(tree[cur][0]);
-        postOrderTraversal(tree[cur][1]);
-        System.out.print((char)(cur+'A'));
+        inorder(binaryTree[curIdx][0]);
+        System.out.print(cur);
+        inorder(binaryTree[curIdx][1]);
+
     }
 
+    static private void postorder(char cur){
+        if(cur == '.'){
+            return;
+        }
+        int curIdx = cur - 'A' + 1;
+        postorder(binaryTree[curIdx][0]);
+        postorder(binaryTree[curIdx][1]);
+        System.out.print(cur);
+
+
+    }
 }
