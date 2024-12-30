4de7c501491f2cf9e402aaf4f09c6a45f5f86b22
leGit-y <99312658+leGit-y@users.noreply.github.com>
2024-12-31 02:30:55 +0900

    [Gold IV] Title: 트리의 지름, Time: 228 ms, Memory: 23372 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -7,7 +7,8 @@ import java.util.StringTokenizer;
 public class Main {
     static List<Node>[] tree;
     static int[] visited;
-    static int answer = 0;
+    static int max = 0;
+    static int farthest = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,7 +16,7 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         tree = new ArrayList[N+1];
         visited = new int[N+1];
-        
+
         for (int i = 1; i <= N; i++) {
             tree[i] = new ArrayList<>();
         }
@@ -29,25 +30,31 @@ public class Main {
             tree[b].add(new Node(a, c));
         }
 
+        // 루트 노드에서 dfs 돌려서 루트에서 가장 먼 farthest 노드 찾기
+        dfs(1, 0);
 
-        for (int i = 1; i <= N; i++) {
-            Arrays.fill(visited,0);
-            visited[i] =  1;
-            dfs(i, 0);
-        }
+        // farthest 노드에서 dfs 돌리면 트리를 가장 긴 경로로 순회하게 됨
+        Arrays.fill(visited,0);
+        dfs(farthest, 0);
 
-        bw.write(answer + "\n");
+
+        bw.write(max + "\n");
         bw.flush();
     }
 
     private static void dfs(int start, int dist){
+        visited[start] = 1;
+        if(max < dist){
+            max = dist;
+            farthest = start;
+        }
+
         for(Node node: tree[start]){
             if (visited[node.e] == 0) {
-                visited[node.e] = 1;
                 dfs(node.e,dist+node.w);
             }
         }
-        answer = Math.max(answer, dist);
+
     }
 
     private static class Node{
