b576ad58b62c4c706fa4c52bb4ccfb77ca3d6c8b
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-01-08 19:03:34 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 18016 ms, Memory: 110352 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -1,60 +1,32 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class Main {
     static int[] arr;
-    static int[] tmp;
+    static int MAX = 1000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        tmp = new int[N];
+        arr = new int[2*MAX+1];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            int idx = Integer.parseInt(br.readLine()) + MAX;
+            arr[idx]++;
         }
 
-        mergeSort(0,N-1);
+        for (int i = 0; i < MAX*2 + 1; i++) {
+            int cnt = arr[i];
+            if (cnt == 0) continue;
+            int num = i - MAX;
+            while(cnt > 0){
+                bw.write(num + "\n");
+                cnt--;
+            }
 
-        for (int i = 0; i < N; i++) {
-            bw.write(arr[i] + "\n");
         }
         bw.flush();
-    }
-
-    private static void mergeSort(int start, int end){
-        if(start >= end) return;
-
-        int mid = (start + end) / 2;
-        mergeSort(start, mid);
-        mergeSort(mid+1, end);
-
-        int first = start;
-        int second = mid + 1;
-        int idx = first;
-
-        while(first <= mid && second <= end){
-            if(arr[first] <= arr[second]){
-                tmp[idx++] = arr[first++];
-            }else{
-                tmp[idx++] = arr[second++];
-            }
-        }
-
-        while(first <= mid){
-            tmp[idx++] = arr[first++];
-        }
-        while(second <= end){
-            tmp[idx++] = arr[second++];
-        }
-
-        for (int i = start; i <= end; i++) {
-            arr[i] = tmp[i];
-        }
-
     }
 
 }
