5d6eb2d08b2a7adced95fc926d1addf23a40da8b
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-01-13 21:06:55 +0900

    [Silver I] Title: 부등호, Time: 232 ms, Memory: 29272 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int N;
+    static char[] A;
+    static String answer = "";
+    static List<String> answerArr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        A = new char[N];
+        answerArr = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = st.nextToken().charAt(0);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            answer="";
+            visited = new boolean[10];
+            dfs(i, 0);
+
+        }
+
+        System.out.println(answerArr.get(answerArr.size() - 1));
+        System.out.println(answerArr.get(0));
+
+    }
+
+    private static void dfs(int cur, int depth){
+        answer += cur;
+        visited[cur] = true;
+
+        if(depth == N) {
+            answerArr.add(answer);
+            return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if(visited[i]) continue;
+            if(A[depth] == '<' && cur > i) continue;
+            if(A[depth] == '>' && cur < i) continue;
+
+            visited[i] = true;
+            dfs(i, depth+1);
+            answer = answer.substring(0,answer.length()-1);
+            visited[i] = false;
+        }
+
+    }
+
+}
