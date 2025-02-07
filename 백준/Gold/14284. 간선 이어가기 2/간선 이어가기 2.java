475b8baf4bc2cf7b01357c904e2ef4cf62fbbfd5
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-02-08 00:03:47 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 424 ms, Memory: 50592 KB -BaekjoonHub

--- 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
@@ -1,77 +1,85 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-class Node implements Comparable<Node>{
-	int node;
-	int cost;
-	Node(int node,int cost){
-		this.node=node;
-		this.cost=cost;
-	}
-	@Override
-	public int compareTo(Node o) {
-		if(this.cost>o.cost)
-			return 1;
-		return -1;
-	}
-}
+
 
 public class Main {
+    private static class Node implements Comparable<Node>{
+        int n;
+        int dist;
 
-	static StringBuilder sb=new StringBuilder();
+        public Node(int n, int dist) {
+            this.n = n;
+            this.dist = dist;
+        }
 
-	static ArrayList<Node> graph[];
-	static int INF=1000000000;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st=new StringTokenizer(br.readLine());
-		int n=Integer.parseInt(st.nextToken());
-		int m=Integer.parseInt(st.nextToken());
-		graph=new ArrayList[n+1];
-		for(int i=1;i<graph.length;i++)
-			graph[i]=new ArrayList<>();
-		for(int i=0;i<m;i++) {
-			st=new StringTokenizer(br.readLine());
-			int node1=Integer.parseInt(st.nextToken());
-			int node2=Integer.parseInt(st.nextToken());
-			int cost=Integer.parseInt(st.nextToken());
-			graph[node1].add(new Node(node2,cost));
-			graph[node2].add(new Node(node1,cost));
-		}
-		st=new StringTokenizer(br.readLine());
-		int s=Integer.parseInt(st.nextToken());
-		int t=Integer.parseInt(st.nextToken());
-		
-		System.out.println(dijkstra(s,t));
-		
-		
-	}
-	public static int dijkstra(int start,int end) {
-		PriorityQueue<Node> pq=new PriorityQueue<>();
-		pq.add(new Node(start,0));
-		boolean visited[]=new boolean[graph.length];
-		int dist[]=new int[graph.length];
-		Arrays.fill(dist,INF);
-		dist[start]=0;
-		while(!pq.isEmpty()) {
-			
-			int nowVertex=pq.poll().node;
-			if(visited[nowVertex]) continue;
-			visited[nowVertex]=true;
-		
-			for(Node next:graph[nowVertex]) {
-				if(dist[next.node]>dist[nowVertex]+next.cost) {
-					dist[next.node]=dist[nowVertex]+next.cost;
-					pq.add(new Node(next.node,dist[next.node]));
-				}
-				
-			}
-			
-			
-			
-		}
-		
-		return dist[end];
-	}
+        @Override
+        public int compareTo(Node o) {
+            return this.dist - o.dist;
+        }
+    }
+
+    static int n;
+    static final int INF = Integer.MAX_VALUE;
+    static List<Node>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        // graph
+        graph = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new LinkedList<>();
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
+
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        System.out.println(dijkstra(s,t));
+    }
+
+    private static int dijkstra(int start, int end){
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, INF);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start,0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if(dist[cur.n] < cur.dist) continue;
+            for (Node next : graph[cur.n]) {
+                int cost = dist[cur.n] + next.dist;
+
+                if(cost < dist[next.n]){
+                    dist[next.n] = cost;
+                    pq.add(new Node(next.n, cost));
+                }
+            }
+        }
+
+        return dist[end];
+
+
+    }
 }
