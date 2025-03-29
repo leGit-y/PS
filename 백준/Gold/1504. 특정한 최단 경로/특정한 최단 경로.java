d1342ab470b5a78a5e8f2c8ccf2f40ec0f796fdb
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-03-29 13:39:13 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 584 ms, Memory: 70544 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,104 +1,107 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
-
-    Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
-
-}
-
 public class Main {
-    static int N, E;
-    static ArrayList<ArrayList<Node>> a; 
-    static int[] dist; 
+    static int[] dist;
     static final int INF = 200000000;
+    static List<Edge>[] graph;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
 
-        a = new ArrayList<>();
-        dist = new int[N + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        Arrays.fill(dist, INF);
-
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
+        // init graph
+        graph = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new LinkedList<>();
         }
 
+        // input
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-            a.get(start).add(new Node(end, weight));
-            a.get(end).add(new Node(start, weight));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            // 양방향 그래프
+            graph[a].add(new Edge(b, c));
+            graph[b].add(new Edge(a, c));
         }
 
-        // 반드시 거쳐야 하는 정점
+        // 반드시 거쳐야 하는 두 정점
         st = new StringTokenizer(br.readLine());
         int v1 = Integer.parseInt(st.nextToken());
         int v2 = Integer.parseInt(st.nextToken());
 
-        // 1 -> v1 -> v2 -> N으로 가는 경우
-        int res1 = 0;
-        res1 += dijkstra(1, v1);
-        res1 += dijkstra(v1, v2);
-        res1 += dijkstra(v2, N);
 
-        // 1 -> v2 -> v1 -> N으로 가는 경우
-        int res2 = 0;
-        res2 += dijkstra(1, v2);
-        res2 += dijkstra(v2, v1);
-        res2 += dijkstra(v1, N);
+        // init dist
+        dist = new int[N+1];
+        int result1 = 0;
+        // dijkstra
+        // 정점 1 -> v2 -> v1 -> N
+        result1 += dijkstra(1, v2);
+        result1 += dijkstra(v2, v1);
+        result1 += dijkstra(v1, N);
 
-        int ans = (res1 >= INF && res2 >= INF) ? -1 : Math.min(res1, res2);
 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        int result2 = 0;
+        // 정점 1 -> v1 -> v2 -> N
+        result2 += dijkstra(1, v1);
+        result2 += dijkstra(v1, v2);
+        result2 += dijkstra(v2, N);
+
+        int answer = (result1 >= INF && result2 >= INF) ? -1 : Math.min(result1, result2);
+
+        sb.append(answer);
+        System.out.println(sb);
+
+
     }
 
-    public static int dijkstra(int start, int end) {
+    private static int dijkstra(int start, int end){
         Arrays.fill(dist, INF);
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[N + 1];
-        pq.offer(new Node(start, 0));
+        PriorityQueue<Edge> pq = new PriorityQueue<>(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return o1.dist - o2.dist;
+            }
+        });
+        
+        pq.add(new Edge(start, 0));
         dist[start] = 0;
 
         while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int cur = curNode.end;
-            if (dist[cur] < curNode.weight) continue;
-
-            for (Node node : a.get(cur)) {
-                int cost = dist[cur] + node.weight;
-                if (dist[node.end] > cost) {
-                    dist[node.end] = cost;
-                    pq.add(new Node(node.end, dist[node.end]));
+            Edge cur = pq.poll();
+            if (dist[cur.node] < cur.dist) continue;
+            for (Edge next : graph[cur.node]) {
+                int cost = dist[cur.node] + next.dist;
+                if(cost < dist[next.node]){
+                    dist[next.node] = cost;
+                    pq.add(new Edge(next.node, cost));
                 }
             }
-
         }
 
         return dist[end];
+
     }
+    
+    private static class Edge {
+        int node;
+        int dist;
+
+        public Edge(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+
+    }
+
+    
 }
-
-
