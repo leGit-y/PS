6c100d36a3325d8139762b4383041f392c227478
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-04-22 21:43:33 +0900

    [Silver II] Title: 좌표 압축, Time: 448 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
+
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int N;
-    set<int> s;
+    vector<int> v;
 
     cin >> N;
 
@@ -15,12 +16,21 @@ int main(){
     for(int i=0;i<N;i++){
         int x;  cin >> x;
         input[i] = x;
-        s.insert(x);
+        v.push_back(x);
     }
 
-    vector<int> v(s.begin(), s.end());
+    sort(v.begin(), v.end());
+
+    // ex) 1 1 1 2 2 2 3  -- unique() --> 1 2 3 ( 2 2 2 3 )   
+    // (괄호) 안은 원래 배열에 저장된 수 그대로.
+    // unique() 반환값은 뒤의 () 괄호의 시작 위치
+    std::vector<int>::iterator begin = unique(v.begin(), v.end());
+
+    // () 괄호 안의 값들 제거
+    v.erase(begin, v.end());
 
     for(int i=0;i<N;i++){
         cout << lower_bound(v.begin(), v.end(), input[i]) - v.begin() << " ";
     }
+
 }
